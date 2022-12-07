45511610b70c09b208d39365963933336b6ae265
Muradova Farangiz [FE16] <102415848+farangiztae@users.noreply.github.com>
2022-12-07 15:12:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+function setInfo()
+ {let student1 = {
+    name: prompt('Введите свое имя'),
+    surname : prompt('Введите свою'),
+    age: prompt('Введите свой возраст'),
+    object: {student1:""
+    }};
+    console.log(student1);
+
+    let student2 = {
+    name: prompt('Введите свое имя'),
+    surname : prompt('Введите свою фамилию'),
+    age : prompt('Введите свой возраст'),
+    object: {student1:""
+    }};
+    console.log(student2);
+
+    let student3 = {
+    name: prompt('Введите свое имя'),
+    surname : prompt('Введите свою фамилию'),
+    age : prompt('Введите свой возраст'),
+    object: {student1:""
+    }};
+    console.log(student3);
+
+    let student4 = {
+    name: prompt('Введите свое имя'),
+    surname : prompt('Введите свою фамилию'),
+    age : prompt('Введите свой возраст'),
+    object: {student1:""
+    }};
+    console.log(student4);
+
+
+    let student5 = {
+        name: prompt('Введите свое имя'),
+        surname : prompt('Введите свою фамилию'),
+        age : prompt('Введите свой возраст'),
+        object: {student1:""
+    }};
+    console.log(student5);
+}
+setInfo();
